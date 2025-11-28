5d547e9b744de313ba5b8ea0868964bc39619d92
Akut-Luna <felixpaulmeyer@gmail.com>
2025-11-28 12:56:01 +0100

    look up tables for iron and silicat

---
+++ make_lookup_tables.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def EoS_Vinet(eta, K0, K0_prime):
+    p = 3*K0 * eta**(2/3) * (1-eta**(-1/3)) * np.exp(3/2 * (K0_prime - 1)*(1-eta**(-1/3)))
+    return p
+
+def EoS_BME(eta, K0, K0_prime):
+    p = 3/2*K0 * (eta**(7/3) - eta**(5/3)) * (1 + 3/4 * (K0_prime - 4) * (eta**(2/3) - 1))
+    return p
+
+def EoS_BME4(eta, K0, K0_prime, K0_prime_prime):
+    p = EoS_BME(eta, K0, K0_prime) + 3/2*K0 * (eta**(7/3) - eta**(5/3)) * 3/8 * K0 * (eta**(2/3) - 1)**2 * (K0*K0_prime_prime + K0_prime*(K0_prime - 7) + 143/9)
+    return p
+
+
+if __name__ == '__main__':
+
+    N = 100
+    rho0 = 1e-6 # Mg/m^3
+    rho1 = 100  # Mg/m^3
+
+    rho_grid = np.linspace(rho0, rho1, N)
+    
+    theta = 1
+    rho_grid = np.linspace(0, 1, N)
+    rho_grid = rho1 * (1 - rho_grid**theta) + 1e-6  # power-law stretched coordinates
+
+    # Iron
+    K0 = 156.2 # GPa
+    K0_prime = 6.08
+    rho0 = 8.30 # Mg/m^3
+    data = np.zeros((N,2))
+    for i, rho in enumerate(rho_grid):
+        eta = rho/rho0
+        p = EoS_Vinet(eta, K0, K0_prime)
+        data[i,0] = p
+        data[i,1] = rho
+
+    data = data[data[:, 0].argsort()] # sort by pressure
+    data = data[data[:, 0] > 0]       # disregard negative pressures
+
+    header = 'p [GPa], rho [Mg/m^3]'
+    np.savetxt(f'data/EoS_Fe/EoS_Fe.csv', data, delimiter=',', header=header)
+
+    plt.figure(figsize=(12,4))
+    ax1 = plt.subplot(1, 2, 1)
+    ax1.plot(data[:,0], data[:,1], '.-')
+    ax1.set_xlabel('p [GPa]')
+    ax1.set_ylabel('rho [Mg/m^3]')
+    ax1.set_title('Iron')
+    ax1.grid(True, alpha=0.3)
+
+    # Silicat
+    K0 = 247 # GPa
+    K0_prime = 3.97 
+    K0_prime_prime = -0.016 # 1/GPa
+    rho0 = 4.10 # Mg/m^3
+    data = np.zeros((N,2))
+    for i, rho in enumerate(rho_grid):
+        eta = rho/rho0
+        p = EoS_BME4(eta, K0, K0_prime, K0_prime_prime)
+        data[i,0] = p
+        data[i,1] = rho
+
+    data = data[data[:, 0].argsort()] # sort by pressure
+    data = data[data[:, 0] > 0]       # disregard negative pressures
+
+    header = 'p [GPa], rho [Mg/m^3]'
+    np.savetxt(f'data/EoS_Si/EoS_Si.csv', data, delimiter=',', header=header)
+
+    ax2 = plt.subplot(1, 2, 2)
+    ax2.plot(data[:,0], data[:,1], '.-')
+    ax2.set_xlabel('p [GPa]')
+    ax2.set_ylabel('rho [Mg/m^3]')
+    ax2.set_title('Silicat')
+    ax2.grid(True, alpha=0.3)
+
+    plt.show()
--- planet_solver_ex_02.py
+++ planet_solver_ex_02.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def itteration_step(r1, r2, p1, m1, C):
+def itteration_step(EoS, r1, r2, p1, m1, C):
     '''
         Parameters:
+            EoS: equ of state, must have arguments in a list [p, t, ...]
             r1: r_i, bigger/outer radius
             r2: r_i+1, smaller/inner radius
             p1: p_i, pressure at r_i
@@ -40,8 +41,8 @@ def itteration_step(r1, r2, p1, m1, C):
         print('')
     # ===========================================
     
-    T = temp_from_pressure(p1, C)
-    rho = rho_from_EoS_ideal_gas(p1, T)
+    T = temp_from_pressure(p1, C) # FOR MONOATOMIC H!!!
+    rho = EoS(p1, T)
     
     m2 = m1 - 4/3 * np.pi * (r1**3 - r2**3) * rho
     p2 = p1 + G * m2 * rho * (1/r2 - 1/r1)
@@ -51,8 +52,43 @@ def itteration_step(r1, r2, p1, m1, C):
 def temp_from_pressure(p, C):
     return (C/(p**(-2/3)))**(3/5)
 
-def rho_from_EoS_ideal_gas(p, T):
+def EoS_ideal_gas(p, T):
+    '''
+        Parameters:
+            p: pressure
+            T: temperature
+        Returns:
+            rho: density
+    '''
     return (p*M_mole)/(R*T)
+
+def EoS_polytropic(p, T):
+    '''
+        Parameters:
+            p: pressure
+            T: temperature
+        Returns:
+            rho: density
+    '''
+    K = 1.96e12
+    return np.sqrt(p/K)
+
+def EoS_analytical_iron(p, T):
+    p_Gpa = p * 1e-10
+    
+    K0 = 156.2 # GPa
+    K0_prime = 6.08
+    rho0 = 8.30 # Mg/m^3
+
+
+def EoS_analytical_silicate(p, T):
+    pass
+
+def EoS_tabulated_H(p, T):
+    pass
+
+def EoS_tabulated_H2O(p, T):
+    pass
 
 def plot_data(data, filename):
     plt.figure(figsize=(12,8))
@@ -92,65 +128,75 @@ def save_data(data, filename):
     header = 'r [cm], m [g], p [dyn/cm^2], T [K], rho [g/cm^3]'
     np.savetxt(f'data/{filename}.csv', data, delimiter=',', header=header)
 
-# ------------------ constants ------------------
-# M_mole =  1.008 # g/mole (monoatomic H)
-M_mole =  2.22   # g/mole https://radiojove.gsfc.nasa.gov/education/educationalcd/Posters&Fliers/FactSheets/JupiterFactSheet.pdf
-R = 8.31434e7   # erg / (K * mole)
-G = 6.67430e-8  # dyn cm^2 / g^2
+def simulate_Jupiter(N, theta):
+    '''
+        Prameters:
+            N: number of steps
+            theta: strech factor -> higher = r_grid is more dense close to the surface
+    '''
 
-# ------------- boundary conditions -------------
-R_mean_Jupiter = 69911   # km
-T_surface_Jupiter = 165  # K
-P_surface_Jupiter = 1    # bar
-M_Jupiter = 1.8982e27    # kg
+    # ------------- boundary conditions -------------
+    R_mean_Jupiter = 69911   # km
+    T_surface_Jupiter = 165  # K
+    P_surface_Jupiter = 1    # bar
+    M_Jupiter = 1.8982e27    # kg
 
-# --------------- convert to cgs ----------------
-R_mean_cgs = R_mean_Jupiter * 1e5        # cm
-T_surface_cgs = T_surface_Jupiter        # K
-P_surface_cgs = P_surface_Jupiter * 1e6  # dyn/cm^2
-M_cgs = M_Jupiter * 1000                 # g
+    # --------------- convert to cgs ----------------
+    R_mean_cgs = R_mean_Jupiter * 1e5        # cm
+    T_surface_cgs = T_surface_Jupiter        # K
+    P_surface_cgs = P_surface_Jupiter * 1e6  # dyn/cm^2
+    M_cgs = M_Jupiter * 1000                 # g
 
-gamma = 5/3
-C_cgs = P_surface_cgs**(1-gamma) * T_surface_cgs**gamma # (dyn * K)/cm^2
+    gamma = 5/3
+    C_cgs = P_surface_cgs**(1-gamma) * T_surface_cgs**gamma # (dyn * K)/cm^2
 
-# ------------------- r_grid --------------------
-'''
- We want to sampe more r values closer to the surface because P changes there more rapidly. 
-'''
-N = 100
-theta = 5                               # strech factor -> higher = more more dense close to the surface
-s = np.linspace(0.0, 1.0, N+1)          # normalized coordinates
-r_grid = R_mean_cgs * (1 - s**theta)    # power-law stretched coordinates
+    # ------------------- r_grid --------------------
+    '''
+    We want to sampe more r values closer to the surface because P changes there more rapidly. 
+    '''
+    s = np.linspace(0.0, 1.0, N+1)          # normalized coordinates
+    r_grid = R_mean_cgs * (1 - s**theta)    # power-law stretched coordinates
 
-# ---------------- prepare data -----------------
-data = np.zeros((N+1,5)) # [r, m, p, T, rho]
+    # ---------------- prepare data -----------------
+    data = np.zeros((N+1,5)) # [r, m, p, T, rho]
 
-data[0,0] = R_mean_cgs
-data[0,1] = M_cgs
-data[0,2] = P_surface_cgs
+    data[0,0] = R_mean_cgs
+    data[0,1] = M_cgs
+    data[0,2] = P_surface_cgs
 
-# --------------- run simulation ----------------
-for i in range(N):
-    r1 = r_grid[i]
-    r2 = r_grid[i+1]
+    # --------------- run simulation ----------------
+    for i in range(N):
+        r1 = r_grid[i]
+        r2 = r_grid[i+1]
 
-    m1 = data[i,1] 
-    p1 = data[i,2]
+        m1 = data[i,1] 
+        p1 = data[i,2]
 
-    m2, p2, T, rho = itteration_step(r1, r2, p1, m1, C_cgs)
+        # m2, p2, T, rho = itteration_step(EoS_ideal_gas, r1, r2, p1, m1, C_cgs)
+        m2, p2, T, rho = itteration_step(EoS_polytropic, r1, r2, p1, m1, C_cgs)
 
-    data[i,3] = T
-    data[i,4] = rho
+        data[i,3] = T
+        data[i,4] = rho
 
-    if i < len(data)-1:
-        data[i+1,0] = r2
-        data[i+1,1] = m2
-        data[i+1,2] = p2
+        if i < len(data)-1:
+            data[i+1,0] = r2
+            data[i+1,1] = m2
+            data[i+1,2] = p2
 
-    # print(f'{i+1}/{N}', end='\r')
+        # print(f'{i+1}/{N}', end='\r')
 
-# ----------------- save data -------------------
-data =  data[:-1] # the last line contains no useful data and can be removed
+    # ----------------- save data -------------------
+    data =  data[:-1] # the last line contains no useful data and can be removed
 
-save_data(data, 'ex_02_Jupiter')
-plot_data(data, 'ex_02_Jupiter')
+    save_data(data, 'ex_02_Jupiter')
+    plot_data(data, 'ex_02_Jupiter')
+
+if __name__ == '__main__':
+
+    # ------------------ constants ------------------
+    # M_mole =  1.008 # g/mole (monoatomic H)
+    M_mole =  2.22   # g/mole https://radiojove.gsfc.nasa.gov/education/educationalcd/Posters&Fliers/FactSheets/JupiterFactSheet.pdf
+    R = 8.31434e7   # erg / (K * mole)
+    G = 6.67430e-8  # dyn cm^2 / g^2
+
+    simulate_Jupiter(N=100, theta=1)
